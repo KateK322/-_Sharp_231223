3ad16b78190c3ac5dce2f0b483dedb9792d46816
Ekaterina <kononova.22@mail.ru>
2024-01-06 23:59:40 +0300

    Task4

---
+++ lesson2/2_4/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа выводит третью с конца цифру заданного числа или сообщает что третей цифры нет
+
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 100) {Console.WriteLine ("False");}
+else {Console.WriteLine ("True" + a / 100 % 10);}
