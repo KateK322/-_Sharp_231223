c893b88c54767af253cd24f0255d3cb4b63d3c09
Ekaterina <kononova.22@mail.ru>
2024-01-06 22:24:53 +0300

    tASK1

---
+++ lesson1/lesson1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Два целых числа и проверяет, является ли первое число квадратом второго
+
+int a = 25;
+int b = 5;
+
+if (b * b == a)
+    Console.WriteLine ("True");
+else
+    Console.WriteLine ("False");
+
+
+Console.Write ("Enter a number;: ");
+string str1 = Console.ReadLine ()!;
+
