1139e1a841ebd45f9a6d686e89a19724d571dffe
Ekaterina <kononova.22@mail.ru>
2024-01-06 23:52:28 +0300

    Task3

---
+++ lesson2/2_3/Program.cs
@@ -0,0 +1,6 @@
+﻿// Принимать на вход 2 числа и выводить является ли второе число кратно первому. Если второе число не кратно выводить остаток
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a % b == 0) {Console.WriteLine("true");}
+else {Console.WriteLine("false" + a % b);}
