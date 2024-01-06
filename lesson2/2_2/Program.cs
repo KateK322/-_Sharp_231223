7d163c6f76534ab0354f73dec99c388357324d70
Ekaterina <kononova.22@mail.ru>
2024-01-06 23:41:41 +0300

    Task2

---
+++ lesson2/2_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Принимает на вход трёхзначное число, воводит вторую цифру в степень равную третей цифре
+
+int a = int.Parse(Console.ReadLine()!);
+int secomndNumber = a / 10 % 10;
+int thirdNumber = a % 10;
+Console.WriteLine(Math.Pow(secomndNumber,thirdNumber));
+
