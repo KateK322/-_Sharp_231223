6c596dbea3ea7e7137682f640736af328b86b136
Ekaterina <kononova.22@mail.ru>
2024-01-14 10:59:50 +0300

    add Lesson4

---
+++ lesson4/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿// 1. Задайте одномерный массив, заполненный случайными
+//    числами. Определите количество простых чисел в этом
+//    массиве.
+
+static void Main ()
+{
+    // Создаем рандом для генерации случайных чисел
+    Random random = new Random ();
+    // Создаем массив из случайных чисел
+    int[] numbers = new int [10];
+    for (int = 0, int < numbers.Length; i++)
+    {
+        numbers[1] = random.Next(1,100);
+    }
+    int count = 0;
+    foreach (var number is numbers);
+    {
+        if (isPrime(number))
+        {
+            count++
+        }
+        Console.WriteLine ("Количество числел в массиве: " + count);
+    }
+}
+static bool isPrime (int number)
+
+
+
+
+
+
+sing System;
+
+class Program
+{
+    static void Main()
+    {
+        //создаем объект Random для генерации случайных чисел
+        Random random = new Random();
+        //создаем массив из 10 случайных чисел
+        int[] numbers = new int[10];
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 100); //генерация случайного числа от 1 до 100
+        }
+        //определение кол-во простых чисел в массиве
+        int count = 0;
+        foreach (var number in numbers)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+        }
+        //вывод результата
+        Console.WriteLine("Количество простых чисел в массиве: "+ count);
+    }
+    //метод для определения является ли число простым
+    static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ lesson4/Task2/Program.cs
@@ -0,0 +1,146 @@
+﻿// 2. Задайте массив из N случайных целых
+//    чисел (N вводится с клавиатуры).
+//    Найдите количество чисел, которые
+//    оканчиваются на 1 и делятся нацело на 7.
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+
+ void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+ int myFunction(int [] arr)
+  {
+int count=0;
+for(int i=0;i<arr.Length;i++){
+    if(arr[i]%7==0 && arr[i]%10==1){
+        count++;
+
+    }
+
+}
+return count;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+int result=myFunction(mass);
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+// Скопировала на семинаре
+ void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+
+// 
+
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // ввод размера массива с клавиатуры
+        Console.WriteLine("Введите размер массива: ");
+        int size = int.Parse(Console.ReadLine()!);
+
+        // ввод диапазона случайных чисел
+        Console.WriteLine("Введите начало диапазона случайных чисел: ");
+        int start = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Введите конец диапазона случайных чисел: ");
+        int stop = int.Parse(Console.ReadLine()!);
+
+        // генерация массива случайных чисел
+        int[] mass = MassNums(size, start, stop);
+        Print(mass);
+
+        // определение количества чисел, которые оканчиваются на 1 и делятся на 7
+        int count = 0;
+        foreach (var number in mass)
+        {
+            if (number % 10 == 1 && number % 7 == 0) // проверка условий
+            {
+                count++;
+            }
+        }
+
+        // вывод результата
+        Console.WriteLine($"Количество чисел, оканчивающихся на 1 и делящихся нацело на 7: {count}");
+    }
+
+    // метод для генерации массива случайных чисел
+    static int[] MassNums(int size, int from, int to)
+    {
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = new Random().Next(from, to + 1); // генерация случайного числа от from до to
+        }
+        return arr;
+    }
+
+    // метод для вывода массива на экран
+    static void Print(int[] arr)
+    {
+        foreach (var num in arr)
+        {
+            Console.Write($"{num} ");
+        }
+        Console.WriteLine();
+    }
+}
